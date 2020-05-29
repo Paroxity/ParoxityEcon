664bf21462711dbb0404f43979069628e7088988
JackMD <jackmtaylor.jmt@gmail.com>
2020-05-30 01:42:31 +0500

    callback hell removed from /eco pay command

--- src/Paroxity/ParoxityEcon/Command/SubCommand/Pay.php
+++ src/Paroxity/ParoxityEcon/Command/SubCommand/Pay.php
@@ -3,19 +3,20 @@ declare(strict_types = 1);
 
 namespace Paroxity\ParoxityEcon\Command\SubCommand;
 
+use Core\Economy\EconomyEngine;
 use CortexPE\Commando\args\FloatArgument;
 use CortexPE\Commando\BaseSubCommand;
 use CortexPE\Commando\constraint\InGameRequiredConstraint;
 use Paroxity\ParoxityEcon\Command\Argument\ParoxityEconPlayerArgument;
+use Paroxity\ParoxityEcon\Database\ParoxityEconQueryIds;
 use Paroxity\ParoxityEcon\ParoxityEcon;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
+use SOFe\AwaitGenerator\Await;
 use function floatval;
 use function is_null;
 
 class Pay extends BaseSubCommand{
-
-	// THIS CLASS IS A PRIME EXAMPLE OF WHAT YOU CALL AS CALLBACK HELL!
 
 	/** @var ParoxityEcon */
 	private $engine;
@@ -44,74 +45,80 @@ class Pay extends BaseSubCommand{
 			return;
 		}
 
-		$engine = $this->engine;
-
 		$username = $args["player"];
 		$money = floatval($args["money"]);
 
-		// gets senders money and check if he has enough money
-		$engine->getAPI()->getMoney($sender->getUniqueId()->toString(), true,
-			function(?float $sendersBalance) use ($sender, $username, $money): void{
-				if(is_null($sendersBalance)){
-					$sender->sendMessage("§cSomething went wrong. Unable to get your money.");
+		Await::f2c(function() use ($sender, $username, $money){
+			$engine = $this->engine;
+			$database = $engine->getDatabase();
+			$sendersUUID = $sender->getUniqueId()->toString();
 
-					return;
-				}
+			// gets senders money and check if he has enough money
+			$sendersData = yield $database->asyncSingleSelect(ParoxityEconQueryIds::GET_BY_UUID, ["uuid" => $sendersUUID]);
+			$sendersBalance = $sendersData["money"];
 
-				if($money > $sendersBalance){
-					$sender->sendMessage("§cYou do not have enough money to perform this transaction.");
+			if(is_null($sendersBalance)){
+				$sender->sendMessage("§cSomething went wrong. Unable to get your money.");
 
-					return;
-				}
-
-				$online = false;
-				$string = $username;
-
-				$player = $this->engine->getServer()->getPlayerExact($username);
-
-				if(!is_null($player) && $player->isOnline()){
-					$online = true;
-					$string = $player->getUniqueId()->toString();
-				}
-
-				// add the money to the targets balance
-				$this->engine->getAPI()->addMoney($string, $money, $online,
-					function(bool $success) use ($sender, $username, $money, $online, $player, $string, $sendersBalance): void{
-						if(!$success){
-							$sender->sendMessage("§cPlayer:§4 $username §ccould not be found.");
-
-							return;
-						}
-
-						// before adding to targets balance, deduct from senders first
-						$this->engine->getAPI()->deductMoney($sender->getUniqueId()->toString(), $money, true,
-							function(bool $success) use ($sender, $player, $username, $string, $online, $money, $sendersBalance): void{
-								if(!$success){
-									$this->engine->getAPI()->deductMoney($string, $money, $online); // remove the money that was added to targets balance
-
-									$sender->sendMessage("§cUnable to perform the transaction. Something went wrong.");
-
-									return;
-								}
-
-								// target balance was added and senders money was deducted. proceed...
-								// get targets updated balance
-								$this->engine->getAPI()->getMoney($string, $online,
-									function(?float $usersBalance) use ($sender, $username, $money, $online, $player, $sendersBalance): void{
-										$unit = ParoxityEcon::getMonetaryUnit();
-
-										if($online){
-											$player->sendMessage("§aPlayer: §2{$sender->getName()} §agave you §6$unit" . $money . "§a. Your new balance is §6$unit" . $usersBalance);
-										}
-
-										$sender->sendMessage("§aSuccessfully paid §6$unit" . "$money §ato §2$username. §aYour balance now is §6$unit" . ($sendersBalance - $money));
-									}
-								);
-							}
-						);
-					}
-				);
+				return;
 			}
-		);
+
+			if($money > $sendersBalance){
+				$sender->sendMessage("§cYou do not have enough money to perform this transaction.");
+
+				return;
+			}
+
+			$online = false;
+			$lookup = ["username" => $username];
+
+			$player = $this->engine->getServer()->getPlayerExact($username);
+
+			if($player instanceof Player && $player->isOnline()){
+				$online = true;
+				$lookup = ["uuid" => $player->getUniqueId()->toString()];
+			}
+
+			$addLookup = $lookup;
+			$addLookup["money"] = $money;
+			$addLookup["max"] = EconomyEngine::getMaxMoney();
+
+			// add the money to the targets balance
+			$result = yield $database->asyncChange($online ? ParoxityEconQueryIds::ADD_BY_UUID : ParoxityEconQueryIds::ADD_BY_USERNAME, $addLookup);
+
+			if($result === 0){
+				$sender->sendMessage("§cPlayer:§4 $username §ccould not be found.");
+
+				return;
+			}
+
+			// before adding to targets balance, deduct from senders first
+			$result = yield $database->asyncChange(ParoxityEconQueryIds::DEDUCT_BY_UUID, ["uuid" => $sendersUUID, "money" => $money]);
+
+			if($result === 0){
+				$deductLookup = $lookup;
+				$deductLookup["money"] = $money;
+
+				// remove the money that was added to targets balance
+				yield $database->asyncChange($online ? ParoxityEconQueryIds::DEDUCT_BY_UUID : ParoxityEconQueryIds::DEDUCT_BY_USERNAME, $deductLookup);
+
+				$sender->sendMessage("§cUnable to perform the transaction. Something went wrong.");
+
+				return;
+			}
+
+			// target balance was added and senders money was deducted. proceed...
+			// get targets updated balance
+			$playersData = yield $database->asyncSingleSelect($online ? ParoxityEconQueryIds::GET_BY_UUID : ParoxityEconQueryIds::GET_BY_USERNAME, $lookup);
+			$playersBalance = $playersData["money"];
+
+			$unit = ParoxityEcon::getMonetaryUnit();
+
+			if($online){
+				$player->sendMessage("§aPlayer: §2{$sender->getName()} §agave you §6$unit" . $money . "§a. Your new balance is §6$unit" . $playersBalance);
+			}
+
+			$sender->sendMessage("§aSuccessfully paid §6$unit" . "$money §ato §2$username. §aYour balance now is §6$unit" . ($sendersBalance - $money));
+		});
 	}
 }
