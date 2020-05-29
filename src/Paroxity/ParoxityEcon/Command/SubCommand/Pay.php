fd4316bfaf0abebf11d3a431ae8e2ef74ce8d6fc
JackMD <jackmtaylor.jmt@gmail.com>
2020-05-30 02:49:18 +0500

    use ParoxityEconAPI::pay() logic

--- src/Paroxity/ParoxityEcon/Command/SubCommand/Pay.php
+++ src/Paroxity/ParoxityEcon/Command/SubCommand/Pay.php
@@ -7,11 +7,10 @@ use CortexPE\Commando\args\FloatArgument;
 use CortexPE\Commando\BaseSubCommand;
 use CortexPE\Commando\constraint\InGameRequiredConstraint;
 use Paroxity\ParoxityEcon\Command\Argument\ParoxityEconPlayerArgument;
-use Paroxity\ParoxityEcon\Database\ParoxityEconQueryIds;
 use Paroxity\ParoxityEcon\ParoxityEcon;
+use Paroxity\ParoxityEcon\ParoxityEconAPI;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
-use SOFe\AwaitGenerator\Await;
 use function floatval;
 
 class Pay extends BaseSubCommand{
@@ -46,78 +45,45 @@ class Pay extends BaseSubCommand{
 		$username = $args["player"];
 		$money = floatval($args["money"]);
 
-		Await::f2c(function() use ($sender, $username, $money){
-			$engine = $this->engine;
-			$database = $engine->getDatabase();
-			$sendersUUID = $sender->getUniqueId()->toString();
+		$this->engine->getAPI()->pay(
+			$sender->getName(),
+			$username,
+			$money,
 
-			// gets senders money and check if he has enough money
-			$sendersData = yield $database->asyncSelect(ParoxityEconQueryIds::GET_BY_UUID, ["uuid" => $sendersUUID]);
+			function(bool $success, ?float $sendersBalance, ?float $receiversBalance, int $errorCode) use ($sender, $username, $money): void{
+				if($success){
+					$unit = ParoxityEcon::getMonetaryUnit();
+					$player = $this->engine->getServer()->getPlayerExact($username);
 
-			if(empty($sendersData)){
-				$sender->sendMessage("§cSomething went wrong. Unable to get your money.");
+					if($player instanceof Player && $player->isOnline()){
+						$player->sendMessage("§aPlayer: §2{$sender->getName()} §agave you §6$unit" . $money . "§a. Your new balance is §6$unit" . $receiversBalance);
 
-				return;
+					}
+
+					$sender->sendMessage("§aSuccessfully paid §6$unit" . "$money §ato §2$username. §aYour balance now is §6$unit" . $sendersBalance);
+
+					return;
+				}
+
+				switch($errorCode){
+					default:
+					case ParoxityEconAPI::ERROR_CAUSE_UNKNOWN:
+						$sender->sendMessage("§cUnable to perform the transaction. Something went wrong.");
+					break;
+
+					case ParoxityEconAPI::ERROR_SENDER_NOT_FOUND:
+						$sender->sendMessage("§cSomething went wrong. Unable to get your money.");
+					break;
+
+					case ParoxityEconAPI::ERROR_SENDER_LOW_ON_BALANCE:
+						$sender->sendMessage("§cYou do not have enough money to perform this transaction.");
+					break;
+
+					case ParoxityEconAPI::ERROR_RECEIVER_NOT_FOUND:
+						$sender->sendMessage("§cPlayer:§4 $username §ccould not be found.");
+					break;
+				}
 			}
-
-			$sendersBalance = $sendersData[0]["money"];
-
-			if($money > $sendersBalance){
-				$sender->sendMessage("§cYou do not have enough money to perform this transaction.");
-
-				return;
-			}
-
-			$online = false;
-			$lookup = ["username" => $username];
-
-			$player = $this->engine->getServer()->getPlayerExact($username);
-
-			if($player instanceof Player && $player->isOnline()){
-				$online = true;
-				$lookup = ["uuid" => $player->getUniqueId()->toString()];
-			}
-
-			$addLookup = $lookup;
-			$addLookup["money"] = $money;
-			$addLookup["max"] = ParoxityEcon::getMaxMoney();
-
-			// add the money to the targets balance
-			$result = yield $database->asyncChange($online ? ParoxityEconQueryIds::ADD_BY_UUID : ParoxityEconQueryIds::ADD_BY_USERNAME, $addLookup);
-
-			if($result === 0){
-				$sender->sendMessage("§cPlayer:§4 $username §ccould not be found.");
-
-				return;
-			}
-
-			// before adding to targets balance, deduct from senders first
-			$result = yield $database->asyncChange(ParoxityEconQueryIds::DEDUCT_BY_UUID, ["uuid" => $sendersUUID, "money" => $money]);
-
-			if($result === 0){
-				$deductLookup = $lookup;
-				$deductLookup["money"] = $money;
-
-				// remove the money that was added to targets balance
-				yield $database->asyncChange($online ? ParoxityEconQueryIds::DEDUCT_BY_UUID : ParoxityEconQueryIds::DEDUCT_BY_USERNAME, $deductLookup);
-
-				$sender->sendMessage("§cUnable to perform the transaction. Something went wrong.");
-
-				return;
-			}
-
-			// target balance was added and senders money was deducted. proceed...
-			// get targets updated balance
-			$playersData = yield $database->asyncSelect($online ? ParoxityEconQueryIds::GET_BY_UUID : ParoxityEconQueryIds::GET_BY_USERNAME, $lookup);
-			$playersBalance = $playersData[0]["money"];
-
-			$unit = ParoxityEcon::getMonetaryUnit();
-
-			if($online){
-				$player->sendMessage("§aPlayer: §2{$sender->getName()} §agave you §6$unit" . $money . "§a. Your new balance is §6$unit" . $playersBalance);
-			}
-
-			$sender->sendMessage("§aSuccessfully paid §6$unit" . "$money §ato §2$username. §aYour balance now is §6$unit" . ($sendersBalance - $money));
-		});
+		);
 	}
 }
